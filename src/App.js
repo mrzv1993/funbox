df23c3019323265abf776e43183e397e8a30a854
Stanislav Morozov <xxmrzv@gmail.com>
2018-07-05 15:21:11 +1000

    create lg layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+const Main = styled.section`
+  padding: 40px;
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+const Border = styled.div`
+  height: 480px;
+  width: 320px;
+  position: relative;
+  border-radius: 8px;
+  background: linear-gradient(to bottom right, transparent 25px, #1698d9 25px);
+  cursor: pointer;
+  margin-right: 24px;
+  margin-top: 24px;
+
+  &:hover {
+    background: linear-gradient(
+      to bottom right,
+      transparent 25px,
+      #eab05a 25px
     );
   }
-}
+`;
+
+const Card = styled.div`
+  background: linear-gradient(to bottom right, transparent 25px, #f2f2f2 25px);
+  height: 472px;
+  width: 312px;
+  color: #000000;
+  border-radius: 6px;
+  position: absolute;
+  top: 4px;
+  left: 4px;
+`;
+
+const Content = styled.p`
+  display: block;
+  margin-left: 40px;
+`;
+
+const App = () => (
+  <Main>
+    <h1>Ты сегодня покормил кота?</h1>
+    <Cards>
+      <Border>
+        <Card>
+          <Content>saasdsadd</Content>
+        </Card>
+      </Border>
+      <Border>
+        <Card>
+          <Content>saasdsadd</Content>
+        </Card>
+      </Border>
+      <Border>
+        <Card>
+          <Content>saasdsadd</Content>
+        </Card>
+      </Border>
+    </Cards>
+  </Main>
+);
 
 export default App;
